4038bc48e21c4cbe21ce01dd5e5e45b28c60b603
Kenneth <kennethyoung.2960@gmail.com>
2024-02-24 22:49:21 -0600

    initial commit

---
+++ JavaScript_foundations_Looping_over_objects_Node_18_1/src/solution.js
@@ -0,0 +1,18 @@
+/*
+  Write each function according to the instructions.
+  
+  When a function's parameters reference `cart`, it references an object that looks like the one that follows.
+  {
+    "Gold Round Sunglasses": { quantity: 1, priceInCents: 1000 },
+    "Pink Bucket Hat": { quantity: 2, priceInCents: 1260 }
+  }
+*/
+
+function calculateCartTotal(cart) {}
+
+function printCartInventory(cart) {}
+
+module.exports = {
+  calculateCartTotal,
+  printCartInventory,
+};
